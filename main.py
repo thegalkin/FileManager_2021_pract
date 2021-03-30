503c8f3f3d415d271667c246fc36316b8011328f
Nik Galkin <53794828+thegalkin@users.noreply.github.com>
2021-03-30 15:44:08 +0300

    --- Auto-Git Commit ---

--- main.py
+++ main.py
@@ -1,92 +1,87 @@
-'''
-Документация
-
-Создание папки - createDir имя_папки
-
-Удаление папки - removeDir имя_папки
-
-Создание файла - createFile имя_файла
-
-Запись в файл - writeToFile имя_файла
-
-Чтение файла - readFile имя_файла
-
-Удаление файла - deleteFile имя_файла
-
-Копирование вставка - copyPaste имя_файла_исходного имя_папки
-
-Вырезание вставка cutPaste имя_файла_исходного имя_папки
-
-Переименование файла renameFile имя_файла
-
-'''
 
 
-from os import chdir, mkdir, pathconf, path, remove, rename, rmdir
+from os import chdir, getcwd, mkdir, pathconf, path, remove, rename, rmdir
 import shutil as sh
 
 
 class FileManager:
-    chosenFolderName = ""
-    pathToFolder = path(chosenFolderName)
+    motherPath = ""
+    
 
-    def __init__(self, folder):
+    def __init__(self):
         with open("settings.txt", "r") as f:
-            self.chosenFolder = f.read()
+            self.motherPath = f.read()
         self.eraseToBaseFolder()
 
     def eraseToBaseFolder(self):
-        chdir(self.pathToFolder)
+        chdir(self.motherPath)
+        print("erasing to base folder")
     
-    def 
+    def moveUp(self):
+        chdir(self.motherPath.parent)
+        print("moving up")
+
+    def openDir(self, name):
+        chdir(path.join(getcwd(), name))
+        print("opening dir...")
 
     def createDir(self, name):
         mkdir(name)
+        print("creating dir")
 
     def deleteDir(self, name):
         rmdir(name)
+        print("deleting dir")
     
     def changeDir(self, name):
         chdir(name)
+        print("changing dir")
     
     def createFile(self, name):
         f = open(name, "w+")
         f.write("")
         f.close()
+        print("creating dir")
     
     def writeToFile(self, name, text):
         with open(name, "a") as f:
             f.write(text)
+        print("writing to file")
     
     def readFile(self, name):
         with open(name, "r") as f:
             print(f.read())
+        print("reading file")
     
     def deleteFile(self, name):
         remove(name)
+        print("deleting file ")
     
     def copyPaste(self, name, whereTo):
-        sh.copyfile(name, path.join(self.pathToFolder, whereTo))
-    
+        sh.copyfile(name, path.join(self.motherPath, whereTo))
+        print("copying ")
+
     def cutPaste(self, name, whereTo):
-        sh.move(name, path.join(self.pathToFolder, whereTo))
-    
+        sh.move(name, path.join(self.motherPath, whereTo))
+        print("cutting ")
     def renameFile(self, oldName, newName):
         rename(oldName, newName)
-    
+        print("renaming")
+
 def cmdCheck(inp, cmdName):
-    if imp == cmdCheck:
+    if inp == cmdName:
         return True
     else:
         return False
 
 
-cmds = []
+cmds = ["", "", ""]
 fm = FileManager()
 while cmds[0] != "exit":
     cmds = input().split(" ")
-    cmd = cmd[0]
-    cmdBody = cmd[1] #если команда короткая
+    print(cmds)
+    cmd = cmds[0]
+    cmdBody = cmds[1] 
     if cmdCheck(cmd, "createDir"):
         fm.createDir(cmdBody)
     if cmdCheck(cmd, "deleteDir"):
@@ -105,4 +100,8 @@ while cmds[0] != "exit":
         fm.cutPaste(cmds[1], cmds[2])
     if cmdCheck(cmd, "renameFile"):
         fm.renameFile(cmds[1], cmds[2])
-
+    if cmdCheck(cmd, "moveUp"):
+        fm.moveUp()
+    if cmdCheck(cmd, "openDir"):
+        fm.openDir(cmdBody)
+    print("cycle end")
